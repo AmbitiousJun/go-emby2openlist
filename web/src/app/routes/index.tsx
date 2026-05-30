31ae57f8526d2e3a58eb110628131fdaa25eb7d0
ambitious <1535820771@qq.com>
2026-05-30 23:16:54 +0800

    feat: implement index page

--- web/src/app/routes/index.tsx
+++ web/src/app/routes/index.tsx
@@ -1,3 +1,74 @@
+import logoImg from "~/assets/logo.png";
+import { FaGithub } from "react-icons/fa";
+import { Cog } from "lucide-react";
+
 export default function Index() {
-    return <div>这是首页默认组件</div>
+  return (
+    <div className="w-full lg:px-48 max-lg:px-12 space-y-6">
+      <img className="w-50 h-50 mx-auto" src={logoImg} alt="logo" />
+
+      <div className="text-4xl font-bold text-center">go-emby2openlist</div>
+
+      <div className="flex flex-wrap gap-2 items-center justify-center">
+        <a href="https://github.com/AmbitiousJun/go-emby2openlist/tree/v2.7.1" target="_blank">
+          <img src="https://img.shields.io/github/v/tag/AmbitiousJun/go-emby2openlist"></img>
+        </a>
+        <a href="https://hub.docker.com/r/ambitiousjun/go-emby2openlist/tags" target="_blank">
+          <img src="https://img.shields.io/docker/image-size/ambitiousjun/go-emby2openlist/v2.7.1"></img>
+        </a>
+        <a href="https://hub.docker.com/r/ambitiousjun/go-emby2openlist/tags" target="_blank">
+          <img src="https://img.shields.io/docker/pulls/ambitiousjun/go-emby2openlist"></img>
+        </a>
+        <a href="https://github.com/AmbitiousJun/go-emby2openlist/releases/latest" target="_blank">
+          <img src="https://img.shields.io/github/downloads/AmbitiousJun/go-emby2openlist/total"></img>
+        </a>
+        <a href="https://goreportcard.com/report/github.com/AmbitiousJun/go-emby2openlist/v2" target="_blank">
+          <img src="https://goreportcard.com/badge/github.com/AmbitiousJun/go-emby2openlist/v2"></img>
+        </a>
+        <img src="https://img.shields.io/github/stars/AmbitiousJun/go-emby2openlist"></img>
+        <img src="https://img.shields.io/github/license/AmbitiousJun/go-emby2openlist"></img>
+      </div>
+
+      <div className="text-center text-lg">
+        Go 语言编写的 Emby + OpenList
+        网盘直链反向代理服务，深度适配阿里云盘转码播放。
+      </div>
+
+      <div className="max-w-[300px] w-full space-y-6 px-4 mx-auto">
+        <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
+          <p className="leading-6 text-center">
+            快速导航
+          </p>
+          <ul>
+            {resources.map(({ href, text, icon }) => (
+              <li key={href}>
+                <a
+                  className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {icon}
+                  {text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
+
+const resources = [
+  {
+    href: "https://github.com/AmbitiousJun/go-emby2openlist",
+    text: "Github",
+    icon: <FaGithub size={24} />,
+  },
+  {
+    href: "https://github.com/AmbitiousJun/go-emby2openlist/blob/v2.7.1/config-example.yml",
+    text: "示例配置",
+    icon: <Cog />,
+  },
+];
--- web/src/app/routes/layout.tsx
+++ web/src/app/routes/layout.tsx
@@ -1,22 +1,119 @@
+import { useEffect, useState } from "react";
 import type { Route } from "./+types/layout";
-import { Welcome } from "../welcome/welcome";
 import { Link, Outlet } from "react-router";
+import { House, Menu, Moon, Sun, TvMinimalPlay } from "lucide-react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Ge2o Web" },
+    { name: "description", content: "go-emby2openlist web application" },
   ];
 }
 
 export default function Layout() {
-  return <div>
-    <Welcome />
-    <div className="flex items-center justify-center">
-      <Link to="sub1">Sub1</Link>
-      &nbsp; | &nbsp;
-      <Link to="sub2">Sub2</Link>
+  const [dark, setDark] = useState(localStorage.getItem("dark") ? true : false);
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      dark ? "dark" : "light",
+    );
+  }, [dark]);
+
+  const setDarkAndSave = (dark: boolean) => {
+    setDark(dark);
+    localStorage.setItem("dark", dark ? "1" : "");
+  };
+
+  const navigateToMediaServerHome = () => {
+    window.location.href = `${window.location.origin}/`;
+  };
+
+  return (
+    <div>
+      <nav>
+        <div className="max-lg:collapse bg-base-200 mb-12 shadow-sm w-full rounded-md px-4">
+          {/* 全屏遮罩 */}
+          <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
+          <label
+            htmlFor="navbar-1-toggle"
+            className="fixed inset-0 hidden max-lg:peer-checked:block"
+          ></label>
+
+          {/* 导航栏主体 */}
+          <div className="collapse-title navbar">
+            {/* 导航栏左侧 */}
+            <div className="navbar-start">
+              <label
+                htmlFor="navbar-1-toggle"
+                className="btn btn-ghost lg:hidden"
+              >
+                <Menu />
+              </label>
+              <button className="btn btn-ghost text-xl">Ge2o Web</button>
+            </div>
+
+            {/* 导航栏内容 */}
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <details>
+                    <summary>接口调用</summary>
+                    <ul className="p-2 bg-base-100 w-60 z-1">
+                      <li>
+                        <button>OpenList 本地目录树</button>
+                      </li>
+                    </ul>
+                  </details>
+                </li>
+              </ul>
+            </div>
+
+            {/* 导航栏右侧 */}
+            <div className="navbar-end space-x-4">
+              {/* 主题切换 */}
+              <label className="flex cursor-pointer gap-2">
+                <Sun />
+                <input
+                  type="checkbox"
+                  className="toggle"
+                  checked={dark}
+                  onChange={(e) => setDarkAndSave(e.target.checked)}
+                />
+                <Moon />
+              </label>
+
+              {/* 回主页 */}
+              <div className="tooltip tooltip-bottom" data-tip="媒体库主页">
+                <button
+                  className="btn btn-ghost btn-circle"
+                  onClick={navigateToMediaServerHome}
+                >
+                  <House />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* 折叠导航栏 */}
+          <div className="collapse-content lg:hidden z-1">
+            <ul className="menu">
+              <li>
+                <button>接口调用</button>
+                <ul>
+                  <li>
+                    <button>OpenList 本地目录树</button>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
     </div>
-    <Outlet />
-  </div>;
+  );
 }
