003f16d34a16a356e5130fc17f1e8d82ed77a7b5
ambitious <1535820771@qq.com>
2026-06-01 21:50:05 +0800

    feat: implement index page

--- web/src/app/components/settings_modal/settings_modal.tsx
+++ web/src/app/components/settings_modal/settings_modal.tsx
@@ -1,8 +1,40 @@
 import { Settings } from "lucide-react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function SettingsModal() {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const [apiSecret, setApiSecret] = useState(
+    localStorage.getItem("api-secret") || "",
+  );
+  const [apiSecretChecking, setApiSecretChecking] = useState(false);
+  const [apiSecretCheckOk, setApiSecretCheckOk] = useState<boolean | null>(null); 
+
+  // 接口密钥变更的时候就自动检测正确性
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      setApiSecretChecking(true);
+      try {
+        // TODO: 后端增加一个校验接口密钥的接口 并补充前端缺失的逻辑
+        setApiSecretCheckOk(true);
+      } finally {
+        setApiSecretChecking(false);
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [apiSecret]);
+
+  const saveAndClose = () => {
+    localStorage.setItem("api-secret", apiSecret);
+    dialogRef.current?.close();
+  };
+
+  let apiSecretInputColor = "";
+  if (apiSecretCheckOk === true) {
+    apiSecretInputColor = "input-success";
+  } else if (apiSecretCheckOk === false) {
+    apiSecretInputColor = "input-error";
+  }
 
   return (
     <>
@@ -16,9 +48,18 @@ export default function SettingsModal() {
         <div className="modal-box">
           <h3 className="font-bold text-lg pb-4">设置选项</h3>
 
-          <label className="input w-full">
+          <label className={`input w-full ${apiSecretInputColor}`}>
             接口密钥
-            <input type="text" className="grow" placeholder="在此输入 config.yaml 中配置的程序密钥" />
+            <input
+              type="text"
+              className="grow"
+              placeholder="在此输入 config.yaml 中配置的程序密钥"
+              value={apiSecret}
+              onChange={(e) => setApiSecret(e.target.value)}
+            />
+            {apiSecretChecking && (
+              <span className="loading loading-dots loading-xs"></span>
+            )}
           </label>
 
           <div className="modal-action">
@@ -26,7 +67,9 @@ export default function SettingsModal() {
               {/* if there is a button in form, it will close the modal */}
               <button className="btn">关闭</button>
             </form>
-            <button className="btn btn-accent">保存</button>
+            <button className="btn btn-accent" onClick={saveAndClose}>
+              保存
+            </button>
           </div>
         </div>
       </dialog>
